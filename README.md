e82ae6a810d807cba1fbe318bfe236f93f532442
capriisun28 <carsun@berkeley.edu>
2024-07-02 12:50:39 -0700

    model plots still weird, updated README & logs

--- README.md
+++ README.md
@@ -3,9 +3,36 @@
 
 README added by Adam; probably good to have this here. 
 
-**Carolyn**: Please update and rewrite as you see fit! A 
-description, but also a section describing how to run the code 
-may also be good for future use.
+## Notes from Carolyn:
+
+### Motivation:
+
+This project builds upon the work of previous undergraduate students with the Webb group (Scott Kenning and Will Pavlick). Previous implementations employed finite element and exact analytical methods to solve the partial differential equation describing membrane deflection in response to a source pressure, implemented simulations of membrane deflection in response to a constant and single frequency pressure function. 
+
+With this model, I simulate membrane responses to an arbitrary pressure function with specified spatial and temporal profiles. This is done via considering membrane vibrations through time, apropos of incident pulses that are effectively instantanous (modelled as dirac delta functions.) Essentially, an instantanous incident pulse (mimcking pulses from a laser beam) serves to impart a momentum to the membrane that drives the membrane to vibrate; in between pulses the membrane freely oscillates. 
+
+ This will be useful for the Webb group–having the ability to simulate the response of a membrane to any arbitrary pressure function will allow for more streamlined setup and design for future experiments. Avenues that might produce interesting results can be much more readily and cost-effectively explored first through simulation, before deploying an experiment. In general, it is useful to have a robust understanding of the physics underlying optomechanical membrane deformations, as there are many useful applications of optomechanical systems, from implementing effective switches in all-optical communications systems, to optical tweezers that trap atoms and nanoparticles and have applications in fields ranging from biology to microfluidics to quantum computing.
+
+### How to run the sims:
+The main script in question is model.py (this may change as I continue refactoring code). To run this script, download model.py and in the correct directory in the terminal's command line, type "python3 model.py". If this doesn't work, try "python model.py".
+If numpy/matplotlib are not yet installed such that running the above command results in an error that looks like: "Import error: No module named numpy" or "Import error: No module named matplotlib.pyplot", the respective libraries can be installed by running "pip3 install numpy" or "pip3 install matplotlib" (if this throws a "pip3 command not found" error, try just "pip" instead of "pip3").
+
+### What the sims plot:
+(If I can get things to work again. AGHHHHHHHHHHHHHH ........ )
+
+plot_displacement: Shows a 2d overview of the membrane and its displacement response at each time step.
+
+plot_displacement_vs_time & plot_avg_displacement_vs_time: Plotting just the magnitude of the displacement and the average magnitude of the displacement as a function of time. (not sure if only one is needed, I have both for now)
+
+plot_cutout_along_plane: Picking a cutout line that runs across the membrane, and plotting the displacement along that cutout
+
+plot_individual_modes: Plotting the individual modes of the displacement response. In this case, only w_1_1 is being plotted as a sanity check.
+
+### What I'd still like to implement:
+
+- Being able to define multiple source pressures, and superimposing the force they impart to the membrane/the responses of the membrane wrt each individual force
+- Being able to define source pressures with spatial profiles that are not only rectangles (perhaps radial)
+- To test: different known membrane responses by plugging in proper parameters to define the membrane
 
 ---
 
--- model.py
+++ model.py
@@ -3,7 +3,7 @@ import matplotlib.pyplot as plt
 from scipy.integrate import solve_ivp
 from scipy.fft import dst, idst
 
-
+"""
 # parameters
 a = 0.005  # length of the membrane in x-direction (m)
 b = 0.005  # length of the membrane in y-direction (m)
@@ -14,12 +14,12 @@ T = h * sigma  # tension (N/m)
 mu = rho * h  # area mass density (kg/m^2)
 eta = 0  # damping coefficient (Pa/(m/s))
 modes = 10 # number of modes to consider    # prob try a lot more modes
-t_max = 3e-5  # simulation time (s)
+t_max = 1e-5  # simulation time (s)
 dt = 1e-6  # time step (s)
 p0 = 1.0  # pressure amplitude # should i adjust this later?
 
 
-"""
+
 # testing old parameters again for debugging purposes
 a = 1.0  # length of the membrane in x-direction (m)
 b = 1.0  # length of the membrane in y-direction (m)
@@ -33,170 +33,224 @@ dt = 0.01  # time step (s)
 p0 = 1.0  # pressure amplitude
 """
 
-# eigenfns
-def phi_mn(x, y, m, n, a, b):
-    return (2 / np.sqrt(a * b)) * np.sin(m * np.pi * x / a) * np.sin(n * np.pi * y / b)
 
-# eigenvalues
-def eigvals(m, n, a, b):
-    return np.sqrt((m * np.pi / a)**2 + (n * np.pi / b)**2)
+class membrane_response:
+    def __init__(self, impulse_times, dt=1e-6, t_max=1e-5, a=0.005, b=0.005, modes=10, h=5e-8, rho=2850, sigma=250e6, alpha=0.1, eta=0):
+        self.impulse_times = impulse_times
+        self.dt = dt
+        self.t_max = t_max
+        self.a = a
+        self.b = b
+        self.h = h # something that is still confusing to me is that we are treating this and plotting it like a 2D membrane, but we also give it a height so as to define tension as such?
+        self.rho = rho # mass density (kg/m^3)
+        self.sigma = sigma # in plane pressure (MPa)
+        self.tension = self.h * self.sigma # tension (N/m)
+        self.alpha = alpha  # example value
+        self.eta = eta  # damping coefficient (Pa/(m/s))
+        self.mu = self.rho * self.h  # area mass density (kg/m^2)
+        self.num_modes = modes
+        self.alpha = self.eta / (2 * self.mu)
+        self.p0 = 1.0  # pressure amplitude, should i adjust this later?
+        self.x = np.linspace(0, a, 100)
+        self.y = np.linspace(0, b, 100)
+        self.X, self.Y = np.meshgrid(self.x, self.y) #check later, mgrid instead?
 
-#  pressure spatial mode coefficients
-def p_smn(m, n, x0, x1, y0, y1, a, b):
-    return (a / (m * np.pi)) * (np.cos(m * np.pi * x0 / a) - np.cos(m * np.pi * x1 / a)) * \
-           (b / (n * np.pi)) * (np.cos(n * np.pi * y0 / b) - np.cos(n * np.pi * y1 / b))
 
-# initial conditions
-wmn = np.zeros((modes, modes))
-wmn_dot = np.zeros((modes, modes))
-#wmn[0, 0] = 1  # testing testing!! just the phi_1_1 mode
 
-# time array
-t = np.arange(0, t_max, dt)
-impulse_times = [0, 1, 2, 3, 4]
-# impulse_times = [0.2, 0.3, 1, 1.1]
-# impulse_times = [1, 3, 4, 7, 9] commented out for debugging
 
-# array to store displacement
-w_total = np.zeros((len(t), 100, 100))
+    # eigenfns
+    def phi_mn(self, x, y, m, n, a, b):
+        return (2 / np.sqrt(a * b)) * np.sin(m * np.pi * x / a) * np.sin(n * np.pi * y / b)
 
-# spatial grid
-x = np.linspace(0, a, 100)
-y = np.linspace(0, b, 100)
-X, Y = np.meshgrid(x, y) #check later, mgrid instead?
+    # eigenvalues
+    def eigvals(self, m, n, a, b):
+        return np.sqrt((m * np.pi / a)**2 + (n * np.pi / b)**2)
 
-# looping through timesteps
-ti = 0
-while ti in range(len(t)):
-    w = np.zeros_like(X)
-    for m in range(1, modes + 1):
-        for n in range(1, modes + 1):
-            k = eigvals(m, n, a, b)
-            omega0_mn = np.sqrt(T * k**2 / mu)
-            alpha = eta / (2 * mu)
-            #print(alpha) #printing alpha to terminal to debug
-            omega_star = np.sqrt(omega0_mn**2 - alpha**2)
-            #print(omega_star/2*np.pi)
-            
-            # free oscillation solution
-            A = wmn[m-1, n-1]
-            B = (wmn_dot[m-1, n-1] + alpha * A) / omega_star
-            wmn[m-1, n-1] = np.exp(-alpha * t[ti]) * (A * np.cos(omega_star * t[ti]) + B * np.sin(omega_star * t[ti]))
-            
-            # impulse handling (for example impulse at ti=1s)
-            if t[ti] in impulse_times:
-                wmn_dot[m-1, n-1] += p0 * p_smn(m, n, 0, a, 0, b, a, b) / mu
-            
-            w += wmn[m-1, n-1] * phi_mn(X, Y, m, n, a, b)
-    
-    w_total[ti] = w
-    ti += 1
+    #  pressure spatial mode coefficients
+    def p_smn(self, m, n, x0, x1, y0, y1, a, b):
+        return (a / (m * np.pi)) * (np.cos(m * np.pi * x0 / a) - np.cos(m * np.pi * x1 / a)) * \
+            (b / (n * np.pi)) * (np.cos(n * np.pi * y0 / b) - np.cos(n * np.pi * y1 / b))
+
+
+    # looping through timesteps
+    """
+    notes to self, probably store true initial conditions in wmn init init or smth, and then set wminit = wm initially then propagate that 
+    """
+    def calculate_response(self):
+        t = np.arange(0, self.t_max, self.dt)
+        w_total = np.zeros((len(t), self.X.shape[0], self.X.shape[1])) #check this, p sure should be right :D
+        w_mn_arr = np.zeros((self.num_modes, self.num_modes)) #this array is just to make it easy to plot/check response of individual modes
+        last_impulse_index = 0
+        A = 0.0
+        B = 0.0
+
+        for t_index in range(len(t)):
+            current_time = t[t_index]
+
+            #w = np.zeros_like(self.X)
+            for m in range(1, self.num_modes + 1):
+                for n in range(1, self.num_modes + 1):
+                    k = self.eigvals(m, n, self.a, self.b)
+                    pS_mn = self.p_smn(m, n, 0, self.a, 0, self.b, self.a, self.b)
+                    omega0_mn = np.sqrt(self.tension * k**2 / self.mu)
+                    #print(self.alpha) 
+                    omega_star = np.sqrt(omega0_mn**2 - self.alpha**2)
+                    #print(omega_star/2*np.pi)
+
+                # Check if current time is at or past the next impulse
+                    if last_impulse_index + 1 < len(self.impulse_times) and current_time >= self.impulse_times[last_impulse_index + 1]:
+                        # Shift time reference
+                        t_shifted = self.impulse_times[last_impulse_index + 1] - self.impulse_times[last_impulse_index]
+
+                        # Calculate initial conditions for wmn and wmn_dot at the time of the current impulse
+                        wmn_init = np.exp(-self.alpha * t_shifted) * (A * np.cos(self.omega_star * t_shifted) + B * np.sin(self.omega_star * t_shifted))
+                        wmn_dot_init_minus = -self.alpha * wmn_init + self.omega_star * (-A * np.sin(self.omega_star * t_shifted) + B * np.cos(self.omega_star * t_shifted))
+
+                        # Apply the velocity jump condition
+                        wmn_dot_init = wmn_dot_init_minus + self.p0 * pS_mn / self.mu
+
+                        # Update coefficients A and B
+                        A = wmn_init
+                        B = (wmn_dot_init + self.alpha * wmn_init) / self.omega_star
+
+                        # Update the most recent impulse index
+                        last_impulse_index += 1
+
+                    # Calculate wmn at the current time with reference to the last impulse
+                    t_shifted = current_time - self.impulse_times[last_impulse_index]
+                    wmn = np.exp(-self.alpha * t_shifted) * (A * np.cos(omega_star * t_shifted) + B * np.sin(omega_star * t_shifted))
+                    w_mn_arr[m - 1,n - 1] = wmn 
+                    w_total[t_index] = wmn * np.sin(np.pi * self.X / self.a) * np.sin(np.pi * self.Y / self.b)
+
+        return t, w_total, w_mn_arr #should i be returning amplitude instead?
+    """
+    # initial conditions
+    wmn = np.zeros((modes, modes))
+    wmn_dot = np.zeros((modes, modes))
+    #wmn[0, 0] = 1  # testing testing!! just the phi_1_1 mode
+
+    # time array
+    t = np.arange(0, t_max, dt)
+    impulse_times = [0]
+    # impulse_times = [0.2, 0.3, 1, 1.1]
+    # impulse_times = [1, 3, 4, 7, 9] commented out for debugging
+
+    # array to store displacement
+    w_total = np.zeros((len(t), 100, 100))
+
+    # spatial grid
+    x = np.linspace(0, a, 100)
+    y = np.linspace(0, b, 100)
+    X, Y = np.meshgrid(x, y) #check later, mgrid instead?
+
+
+    ti = 0
+    while ti in range(len(t)):
+        w = np.zeros_like(X)
+        for m in range(1, modes + 1):
+            for n in range(1, modes + 1):
+                k = eigvals(m, n, a, b)
+                omega0_mn = np.sqrt(T * k**2 / mu)
+                alpha = eta / (2 * mu)
+                #print(alpha) 
+                omega_star = np.sqrt(omega0_mn**2 - alpha**2)
+                #print(omega_star/2*np.pi)
+                
+                # free oscillation solution
+                A = wmn[m-1, n-1]
+                B = (wmn_dot[m-1, n-1] + alpha * A) / omega_star
+                wmn[m-1, n-1] = np.exp(-alpha * t[ti]) * (A * np.cos(omega_star * t[ti]) + B * np.sin(omega_star * t[ti]))
+                
+                # impulse handling (for example impulse at ti=1s)
+                if t[ti] in impulse_times:
+                    wmn_dot[m-1, n-1] += p0 * p_smn(m, n, 0, a, 0, b, a, b) / mu
+                
+                w += wmn[m-1, n-1] * phi_mn(X, Y, m, n, a, b)
+        
+        w_total[ti] = w
+        ti += 1
+        """
+    def plot_displacement(self, w_total, t):
+        plt.figure(figsize=(10, 6))
+        for ti in range(len(t)): # adjust later, have the factor that len(t) is divided by depend on time steps
+            plt.contourf(self.X, self.Y, w_total[ti], cmap='magma')
+            plt.colorbar()
+            plt.xlabel('x')
+            plt.ylabel('y')
+            plt.title(f'Displacement at t = {t[ti]:.2f} s')
+            plt.savefig(f'displacement_{ti}.png', format="png")
+            plt.show()
+
+
+    # plotting the avg displacement as a function of time
+    def plot_avg_displacement_vs_time(w_total, t):
+        avg_displacement = np.mean(w_total, axis=(1, 2)) # this is because w_total is an array [# time steps, # x's, # y's]
+        plt.plot(t, avg_displacement)
+        plt.xlabel('Time')
+        plt.ylabel('Average Displacement (w)')
+        plt.title('Average Displacement vs. Time')
+        plt.savefig(f'avg_displacement_v_time.png', format="png")
+        plt.show()
+
+    # plotting the displacement as a function of time
+    def plot_displacement_vs_time(self, w, t):
+        plt.figure()
+        for i in range(len(self.x)):
+            for j in range(len(self.y)):
+                plt.plot(t, w[i, j, :], label=f'Point ({self.x[i]:.2f}, {self.y[j]:.2f})')
+        plt.xlabel('Time (s)')
+        plt.ylabel('Displacement')
+        plt.title('Displacement vs Time')
+        plt.legend()
+        plt.savefig('displacement_vs_time.png', format="png")
+        plt.show()
+
+
+    # plotting the displacement along a given plane (e.g., y = b/2)
+    def plot_cutout_along_plane(self, w_total, plane='y', value=0.5):
+        plt.figure()
+        if plane == 'y':
+            idx = (np.abs(self.y - value)).argmin()
+            for ti in range(len(t)):
+                plt.plot(self.x, w_total[ti, idx, :], label=f'Time = {t[ti]:.2f}s')
+            plt.xlabel('Distance along membrane (x)')
+        elif plane == 'x':
+            idx = (np.abs(self.x - value)).argmin()
+            for ti in range(len(t)):
+                plt.plot(self.y, w_total[ti, :, idx], label=f'Time = {t[ti]:.2f}s')
+            plt.xlabel('Distance along membrane (y)')
+        plt.ylabel('Displacement (w)')
+        plt.title(f'Displacement along {plane}={value}')
+        plt.legend()
+        plt.savefig(f'displacement_{plane}_{value}_cutout_{ti}.png', format="png")
+        plt.show()
+
+    # plot individual modes over time (just w_1_1)
+    def plot_individual_modes(self, w_mn_arr, t):
+        plt.figure()
+        plt.plot(t, w_mn_arr[0,0], label='w_1_1') 
+        plt.xlabel('Time')
+        plt.ylabel('Displacement')
+        plt.title('Individual Modes Over Time')
+        plt.legend()
+        plt.savefig('individual_modes_over_time.png', format="png")
+        plt.show()
+
+
+# calling membrane response
+impulse_times = [0]
+deflection = membrane_response(impulse_times)
+t, w_total, w_mn_arr = deflection.calculate_response()
 
 # plotting the results  #the plots take a bit to run
 # this plot should be the main membrane response
-plt.figure(figsize=(10, 6))
-for ti in range(0, len(t), int(len(t)/10)): # adjust later, have the factor that len(t) is divided by depend on time steps
-    plt.contourf(X, Y, w_total[ti], levels=20, cmap='viridis') #here
-    plt.colorbar()
-    plt.title(f'Membrane Displacement Response at Time = {t[ti]:.2f} s')
-    plt.xlabel('x')
-    plt.ylabel('y')
-    plt.show()
-
-
-# plotting the displacement as a function of time
-def plot_displacement_vs_time(w_total, t, x, y):
-    avg_displacement = np.mean(w_total, axis=(1, 2)) # this is because w_total is an array [# time steps, # x's, # y's]
-    plt.plot(t, avg_displacement)
-    plt.xlabel('Time')
-    plt.ylabel('Average Displacement (w)')
-    plt.title('Average Displacement vs. Time')
-    plt.savefig(f'avg_displacement_v_time.png', format="png")
-    plt.show()
-
-# plotting the displacement along a given plane (e.g., y = b/2)
-def plot_cutout_along_plane(w_total, x, y, plane='y', value=0.5):
-    plt.figure()
-    if plane == 'y':
-        idx = (np.abs(y - value)).argmin()
-        for ti in range(len(t)):
-            plt.plot(x, w_total[ti, idx, :], label=f'Time = {t[ti]:.2f}s')
-        plt.xlabel('Distance along membrane (x)')
-    elif plane == 'x':
-        idx = (np.abs(x - value)).argmin()
-        for ti in range(len(t)):
-            plt.plot(y, w_total[ti, :, idx], label=f'Time = {t[ti]:.2f}s')
-        plt.xlabel('Distance along membrane (y)')
-    plt.ylabel('Displacement (w)')
-    plt.title(f'Displacement along {plane}={value}')
-    plt.legend()
-    plt.savefig(f'displacement_{plane}_{value}_cutout_{ti}.png', format="png")
-    plt.show()
-
-# plot individual modes over time (just w_1_1, w_1_2)
-def plot_individual_modes(wmn, t):
-    plt.figure()
-    plt.plot(t, wmn[0, :], label='w_1_1')
-    plt.plot(t, wmn[1, :], label='w_1_2')
-    plt.xlabel('Time')
-    plt.ylabel('Displacement')
-    plt.title('Individual Modes Over Time')
-    plt.legend()
-    plt.savefig('individual_modes_over_time.png', format = "png")
-    plt.close()
-
-
-
-    plt.savefig(f'displacement_{plane}_{value}_cutout_{ti}.png', format="png")
-    plt.show()
-
-# plot individual modes over time (just w_1_1, w_1_2)
-def plot_individual_modes(wmn, t):
-    plt.figure()
-    plt.plot(t, wmn[0, 0, :], label='w_1_1')
-    plt.plot(t, wmn[:,
+deflection.plot_displacement(w_total, t)
 # calls the plotting fn for displacement v time
-plot_displacement_vs_time(w_total, t, x, y)
-
+deflection.plot_displacement_vs_time(w_total, t)
+# calls the plotting fn for avg displacement v time
+deflection.plot_avg_displacement_vs_time(w_total, t)
 # calls the plotting fn for cutout of the response
-plot_cutout_along_plane(w_total, x, y, plane='y', value=b/2)
-
+cutout_line = (deflection.b)/2
+deflection.plot_cutout_along_plane(w_total, plane='y', value=cutout_line)
 # uncomment to check individual modes of the displacement response
-plot_individual_modes(wmn, t,)
-
-"""
-# ignore this commented part this is just me referrring to will's code
-# dimensions of membrane
-# Should be the same, this is a square membrane
-width = 0.005
-height = 0.005
-# associated membrane quantities
-sigma = 250e6 
-eta = 1
-rho = 2850
-h = 100e-9
-# number of sections of membrane in the x (nx) and y (ny) directions
-nx = 21
-ny = 21
-
-'''
-Here are some ways of creating a time dependent forcing function. The 'frequency' variable is useful for simulating
-Gaussian functions or eigenfunction pressure functions at a given sinusoidal time depedence. Some examples are commented
-out since only one ff function should be used.
-
-frequency = 1 #ask if this i should have based on webb model
-# ff = lambda x, y, t: np.sin(2 * np.pi * 200 * x) * np.sin(2 * np.pi * 100 * y) + np.sin(2 * np.pi * 100 * x) * np.sin(
-#     2 * np.pi * 100 * y) + np.sin(2 * np.pi * 300 * x) * np.sin(2 * np.pi * 200 * y) * np.sin(2 * np.pi * frequency * t)
-
-# ff = lambda x, y, t: ( np.sin(2 * np.pi * 100 * x) * np.sin(
-#     2 * np.pi * 100 * y)) * np.sin(2 * np.pi * frequency * t)
-
-
-ff = lambda x, y, t: (np.sin(2 * np.pi * 100 * x) * np.sin(
-    2 * np.pi * 100 * y)) * np.sin(2 * np.pi * frequency * t)
-'''
-
-frequency = 1
-
-use will's ffs in imesolver.py for verifications
-"""
+deflection.plot_individual_modes(w_mn_arr, t)
