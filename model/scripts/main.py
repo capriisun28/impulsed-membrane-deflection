de6a7baac1b848c169440b89cdb380291a043b58
capriisun28 <carsun@berkeley.edu>
2024-07-19 14:26:30 -0700

    added more impulse_times options for trying to excite membrane at res freq for mode (6,5)

--- model/scripts/main.py
+++ model/scripts/main.py
@@ -9,10 +9,12 @@ Note: at least 1 impulse_times array must be uncommented and one initialization 
 
 ### test case 1 (ideal parameters, velocity vs time graph looks weird, continuity on displacement graph also looks odd)
 
-impulse_times = [0]
+#impulse_times = [0]
 #impulse_times = [0, 1e-6, 2e-6, 3e-6]
 #impulse_times = [0, 9e-6, 2e-5, 3.8e-5]
-deflection = membrane_response(impulse_times, dt=1e-7, t_max=5e-5, eta=10, modes=30)
+#impulse_times = [0, 5e-6, 10e-6, 16e-6, 21e-6, 27e-6]
+impulse_times = [0, 5.407e-6, 10.814e-6, 16.221e-6, 21.628e-6, 27.035e-6] #corresponding to exciting mode (6,5) at res freqs
+deflection = membrane_response(impulse_times, dt=1e-8, t_max=3e-5, eta=10, modes=10)
 
 ### test case 2 (used for easier debugging purposes (less terminal output))
 
@@ -27,21 +29,24 @@ plotting.plot_point_deflection_vs_time(w_total, t, a/2, b/2, a, b) # just plotti
 
 ### calls the plotting fn for displacement v time
 ### commented out for now as this runs really slowly
-plotting.plot_displacement_vs_time(w_total, t, x, y)
+#plotting.plot_displacement_vs_time(w_total, t, x, y)
 
 ### calls the plotting fn for avg displacement v time
 #plotting.plot_avg_displacement_vs_time(w_total, t)
 
 ### calls the plotting fn for cutout of the response
-cutout_line = (deflection.b)/2
-plotting.plot_cutout_along_plane(t, x, y, w_total, plane='y', value=cutout_line)
+#cutout_line = (deflection.b)/2
+#plotting.plot_cutout_along_plane(t, x, y, w_total, plane='y', value=cutout_line)
 #plotting.plot_cutout_along_plane_at_timesteps(t, x, y, w_total, plane = 'y', value=cutout_line)
 
 ### uncomment to check individual modes of the displacement response and corresponding velocity responses
-#plotting.plot_individual_modes(w_mn, t, 0, 0)
-#plotting.plot_velocity_imparted_over_time(w_mn_dot_minus, t, 0, 0)
-#plotting.plot_velocity_of_mode_over_time(w_mn_dot, t, 0, 0)
-#plotting.plot_individual_modes(w_mn, t, 9, 9)
+plotting.plot_individual_modes(w_mn, t, 1, 1)
+plotting.plot_velocity_imparted_over_time(w_mn_dot_minus, t, 1, 1)
+plotting.plot_velocity_of_mode_over_time(w_mn_dot, t, 1, 1)
+plotting.plot_individual_modes(w_mn, t, 6, 5)
+plotting.plot_velocity_imparted_over_time(w_mn_dot_minus, t, 6, 5)
+plotting.plot_velocity_of_mode_over_time(w_mn_dot, t, 6, 5)
+plotting.plot_individual_modes(w_mn, t, 9, 9)
 #plotting.plot_velocity_imparted_over_time(w_mn_dot_minus, t, 9, 9)
 #plotting.plot_velocity_of_mode_over_time(w_mn_dot, t, 9, 9)
 
